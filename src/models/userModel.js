2e336560cb320d7c7fadfa4e009e8b0913d46233
SihamBen <siham.bendaia37@gmail.com>
2021-01-16 14:52:18 +0100

    feat(models):user model

---
+++ src/models/userModel.js
@@ -0,0 +1,54 @@
+import { Schema,model, Error } from "mongoose";
+import bcrypt from "bcryptjs";
+const JWT = require("jsonwebtoken");
+
+
+
+const userSchema = new Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "USER" },
+    firstName: String,
+    lastName: String,
+    dateOfBirth: String,
+    occupation: String,
+    phoneNumber: Object, // { dialCode: string , number : string }
+    identityCardNumber: String,
+    address: String,
+    zipCode: String,
+    city: String,
+    country: String,
+  },
+  { timestamps: true }
+);
+
+userSchema.set("toJSON", {
+  transform: (doc, ret, opt) => {
+    delete ret["password"];
+    return ret;
+  },
+});
+
+userSchema.pre("save", function (next) {
+  if (this.isNew) {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+  }
+  next();
+});
+
+userSchema.methods.validPassword = function (inputedPassword: string) {
+  return bcrypt.compareSync(inputedPassword, this.password);
+};
+
+userSchema.methods.getJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.ACCESS_TOKEN_SIGNATURE_KEY);
+};
+
+userSchema.methods.hashPassword = function () {
+  this.password = bcrypt.hashSync(this.password, 8);
+};
+
+module.exports = model("User", userSchema);
